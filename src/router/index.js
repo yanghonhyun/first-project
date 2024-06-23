f99f53ebe17871004fd85356a58a0c4974e498eb
yang <1090761787@qq.com>
2024-06-24 00:34:30 +0800

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,8 @@ const router = createRouter({
           component: () => import('@/views/layout/musicList.vue')
         }
       ]
-    }
+    },
+    { path: '/login', component: () => import('@/views/login/login.vue') }
   ]
 })
 
---
+++ src/views/login/login.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { ref } from 'vue'
+const ruleForm = ref({
+  username: '',
+  password: ''
+})
+//用户名以及密码规则
+const rule = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 10, message: '用户名长度为3-10个字符', trigger: 'blur' },
+    {
+      pattern: /^[0-9a-zA-Z]{6,15}$/,
+      message: '用户名不能为空字符',
+      trigger: 'blur'
+    }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^[0-9a-zA-Z]{6,15}$/,
+      message: '密码长度为6-15位的字母或数字',
+      trigger: 'blur'
+    }
+  ]
+}
+//防止用户多次点击，频繁触发
+const ruleFormRef = ref(null)
+const get = ref(true)
+let time = ref(6)
+let timer = ''
+const getCode = () => {
+  get.value = false
+  if (timer) {
+    return
+  }
+  timer = setInterval(() => {
+    time.value--
+    if (time.value === 0) {
+      clearInterval(timer)
+      timer = ''
+      time.value = 6
+      get.value = true
+    }
+  }, 1000)
+}
+</script>
+<template>
+  <div class="container">
+    <el-col :span="12">
+      <el-form status-icon :model="ruleForm" :rules="rule" ref="ruleFormRef">
+        <el-form-item prop="username">
+          <el-input
+            type="text"
+            placeholder="请输入用户名"
+            v-model="ruleForm.username"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="text"
+            placeholder="请输入密码"
+            v-model="ruleForm.password"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            type="text"
+            placeholder="请输入验证码"
+            class="code"
+          ></el-input>
+          <span @click="getCode">{{
+            get ? '获取验证码' : time + '秒后获取'
+          }}</span>
+        </el-form-item>
+        <el-form-item>
+          <el-button>注册</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </div>
+</template>
+<style scoped>
+.code {
+  width: 600px;
+}
+.container {
+  width: 600px;
+  height: 800px;
+  background-color: aliceblue;
+  border-radius: 10px;
+  margin: 100px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
